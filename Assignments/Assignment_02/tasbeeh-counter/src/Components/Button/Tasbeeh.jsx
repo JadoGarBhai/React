be8f6b8ba92e1fa3634561706ca114dd70a02ceb
JadoGarBhai <sikandernawaz35@gmail.com>
2023-09-04 23:01:53 +0500

    'update'

--- Assignments/Assignment_02/tasbeeh-counter/src/Components/Button/Tasbeeh.jsx
+++ Assignments/Assignment_02/tasbeeh-counter/src/Components/Button/Tasbeeh.jsx
@@ -2,11 +2,28 @@ import styles from "./Tasbeeh.module.css";
 
 const Tasbeeh = (data) => {
   return (
-    <div
-      className={`${styles.btn} btn btn-danger d-flex justify-content-between align-items-center mb-2`}
-    >
-      <div>{data.name}</div>
-      <p>Popular</p>
+    <div>
+      <div
+        className={`${styles.btn} btn btn-danger d-flex justify-content-between align-items-center mb-2`}
+      >
+        <div>{data.name}</div>
+        <p>Popular</p>
+      </div>
+
+      {/* HIDDEN FEATURES.
+      <div className={`${styles.hidden} ${styles.bg}`}>
+        <div className="btn btn-warning col-9 mt-5" onClick={reset}>
+          RESET
+        </div>
+
+        <div className="col-12 mt-5" style={{ color: "#fff" }}>
+          <h2>Tasbeeh</h2>
+        </div>
+
+        <div className={`${styles.mt} btn btn-danger mt-5 col-4`} onClick={add}>
+          <p>{counter}</p>
+        </div>
+      </div> */}
     </div>
   );
 };
--- Assignments/Assignment_02/tasbeeh-counter/src/Components/LandingPage/LandingPage.jsx
+++ Assignments/Assignment_02/tasbeeh-counter/src/Components/LandingPage/LandingPage.jsx
@@ -1,25 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./LandingPage.module.css";
 import Tasbeeh from "../Button/Tasbeeh";
 
 function LandingPage() {
+  const [counter, setCounter] = useState(0);
+
+  const add = () => {
+    setCounter(counter + 1);
+  };
+
+  const reset = () => {
+    setCounter(0);
+  };
+
   return (
     <div>
       <div className="col-xl-4 col-sm-12">
         <h1>Tasbeeh Counter</h1>
       </div>
 
-      <div className={`${styles.mainBody} col-xl-4 col-sm-12 pb-3`}>
-        <h2 className="col-12">Popular Tasbeeh</h2>
-        <div className="container">
-          <div className={styles.tesbeh}>
-            <Tasbeeh name={"Allah o Akbar"} />
-            <Tasbeeh name={"SubhanAllah"} />
-            <Tasbeeh name={"Astagfirullah"} />
-            <Tasbeeh name={"Alhamdulillah"} />
+      <div className={`${styles.mainBody} col-xl-4 col-sm-12`}>
+        <div className={``}>
+          <h2 className="col-12 pt-3">Popular Tasbeeh</h2>
+          <div className="container">
+            <div className={styles.tesbeh}>
+              <Tasbeeh name={"Allah o Akbar"} />
+              <Tasbeeh name={"SubhanAllah"} />
+              <Tasbeeh name={"Astagfirullah"} />
+              <Tasbeeh name={"Alhamdulillah"} />
+            </div>
+
+            <div className="btn btn-danger mt-3 col-12 mb-3">
+              + CREATE NEW TASBEEH
+            </div>
+          </div>
+        </div>
+
+        {/* HIDDEN FEATURES. */}
+        <div className={`${styles.hidden} ${styles.bg}`}>
+          <div className="btn btn-warning col-9 mt-5" onClick={reset}>
+            RESET
           </div>
 
-          <div className="btn btn-danger mt-3 col-12">+ CREATE NEW TASBEEH</div>
+          <div className="col-12 mt-5" style={{ color: "#fff" }}>
+            <h2>Tasbeeh</h2>
+          </div>
+
+          <div
+            className={`${styles.mt} btn btn-danger mt-5 col-4`}
+            onClick={add}
+          >
+            <p>{counter}</p>
+          </div>
         </div>
       </div>
     </div>
