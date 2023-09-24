f5a7ca4fa8b55707294d27772fb3c0d63fdab639
JadoGarBhai <sikandernawaz35@gmail.com>
2023-09-24 23:03:43 +0500

    'calculatior'

--- Assignments/Assignment_03/calculator/src/App.js
+++ Assignments/Assignment_03/calculator/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <input type="text" name="result1" id="result1" readOnly />
+        <input type="text" name="result2" id="result2" readOnly />
+      </div>
+      <button>9</button>
+      <button>8</button>
+      <button>7</button>
+      <button>6</button>
+      <button>5</button>
+      <button>4</button>
+      <button>3</button>
+      <button>2</button>
+      <button>1</button>
+      <button>0</button>
+    </>
+  );
 }
 
 export default App;
